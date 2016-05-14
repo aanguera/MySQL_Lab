6e1774f8b5d37318092154140463d18ac160b9e7
root <root@raspberrypi.(none)>
2016-05-14 14:33:31 +0200

    Modifient the style in order to be more readable

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 	$page_tittle = 'MySQL Laboratory';
 	include('includes/header.html');
-?>
-<h4>About the Lab</h4>
-<p>This is a laboratory to connect databases and execute MySQL queries</p>
-<?php
-	include('includes/footer.html');
+
+	echo "<h4>About the Lab</h4>";
+	echo "<p>This is a laboratory to connect databases and execute MySQL queries</p>";
+
+include('includes/footer.html');
 ?>
